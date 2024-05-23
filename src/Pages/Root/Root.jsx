88f8fec0d8e45b43bf9f0158c261c9a26c1325cc
mehedifiz <mehedi.shanto.contract@gamil.com>
2024-05-23 12:55:06 +0600

    first commit

---
+++ src/Pages/Root/Root.jsx
@@ -0,0 +1,12 @@
+import Nav from "../../Compos/Nav/Nav";
+
+const Root = () => {
+    return (
+        <div>
+          <Nav></Nav>
+            <h1 className="text-4xl">this is home page </h1>
+        </div>
+    );
+};
+
+export default Root;
